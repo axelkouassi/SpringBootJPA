4026cc096209db4dc9d458f235169e001458cf65
Axel Kouassi <ak659@njit.edu>
2020-05-08 15:22:08 -0400

    POST and GET requests in POSTMAN

--- SpringBootJPA/src/main/java/com/example/demo/controller/AlienController.java
+++ SpringBootJPA/src/main/java/com/example/demo/controller/AlienController.java
@@ -5,16 +5,20 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.AlienRepo;
 import com.example.demo.model.Alien;
 
-@Controller
+@RestController
 public class AlienController {
 	
 	@Autowired
@@ -25,34 +29,30 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
-	@RequestMapping("/addAlien")
-	public String addAlien(Alien alien) {
+	@PostMapping(path="/alien", consumes = {"application.json"})
+	public Alien addAlien(@RequestBody Alien alien) {
 		repo.save(alien);
-		return "home.jsp";
+		return alien;
 	}
 	
-	@RequestMapping("/aliens")
-	@ResponseBody
-	public List<Alien> getAliens(/*@RequestParam int aid*/) {
-		
-		/*ModelAndView mv = new ModelAndView("showAlien.jsp");
-		System.out.println("findByTech: " + repo.findByTech("Java"));
-		System.out.println("findByAidGreaterThan: " + repo.findByAidGreaterThan(104));
-		System.out.println("findByTechSorted: " + repo.findByTechSorted("Java"));
-		mv.addObject(alien);
-		Alien alien = repo.findById(aid).orElse(new Alien());
-		return mv;*/
-		
-		return repo.findAll();
-			
+	@GetMapping(path="/aliens")
+	public List<Alien> getAliens() {
+		return repo.findAll();		
 	}
 	
-	@RequestMapping(path="/alien/{aid}", produces= {"application/xml"})
-	@ResponseBody
+	@RequestMapping("/alien/{aid}")
 	public Optional<Alien> getAlien(@PathVariable("aid")int aid) {
-		
-		return repo.findById(aid);
-			
+		return repo.findById(aid);		
 	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
